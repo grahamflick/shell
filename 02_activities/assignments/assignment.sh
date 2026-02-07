d0194abaed8e3b3fc9caea99a5aa0409b1cb7f21
grahamflick <graham.flick@nyu.edu>
2026-02-07 17:39:47 -0400

    Q8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -56,7 +56,7 @@ rm data/raw/*ipaddr_*
 rm data/processed/user_logs/*ipaddr*
 
 # # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-# find data/processed > data/inventory.txt
+find data/processed > data/inventory.txt
 
 ###########################################
 

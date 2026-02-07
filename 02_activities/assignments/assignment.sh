b7a1899b6b940a9b71cbb83a19645066f01c28d8
grahamflick <graham.flick@nyu.edu>
2026-02-07 17:38:45 -0400

    Q7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,8 +52,8 @@ cp data/raw/event_*.log data/processed/event_logs
 
 # # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 # # rf -rf ./data
-# rm data/raw/ipaddr_*
-# rm data/processed/user_logs/*_ipaddr_*
+rm data/raw/*ipaddr_*
+rm data/processed/user_logs/*ipaddr*
 
 # # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 # find data/processed > data/inventory.txt
